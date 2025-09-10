dfa22f64a02e4151aa69fcdfbe756b5b3685148b
prakashharika <prakashbsc376@gmail.com>
2025-09-10 22:35:20 +0530

    Update BuyerController.php

--- app/Http/Controllers/BuyerController.php
+++ app/Http/Controllers/BuyerController.php
@@ -1,1 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Buyer;
+use Illuminate\Http\Request;
+
+class BuyerController extends Controller
+{
+    public function index()
+    {
+        $buyers = Buyer::all(); // Or use pagination: Buyer::paginate(10)
+
+        return view('admin.usertable', compact('buyers'));
+    }
+}
 
